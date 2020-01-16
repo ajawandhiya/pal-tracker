8f0948a6b1ca2323cdd55e3a57ea37ae074721d3
unknown <GDCUser1@LTUS54274.cts.com>
2020-01-16 13:26:01 -0600

    CI/CD build again

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -1,9 +1,8 @@
 package test.pivotal.pal.tracker;
 
-import org.junit.Test;
-
-import java.util.Map;
 import io.pivotal.pal.tracker.EnvController;
+import org.junit.Test;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
