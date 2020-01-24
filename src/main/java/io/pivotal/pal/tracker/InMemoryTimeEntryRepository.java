f37c2db4248a9e7215e450360d07c28b7534177b
ajawandhiya <abhishek.jawandhiya@cognizan.com>
2020-01-24 15:47:11 -0600

    Add task for migrating databases
    # Conflicts:
    #	.travis.yml

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -4,63 +4,59 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class InMemoryTimeEntryRepository implements TimeEntryRepository{
-
-    private TimeEntry timeEntry = new TimeEntry();
-    private HashMap<Long, TimeEntry> timeEntryHashMap = new HashMap<Long, TimeEntry>() ;
-    private long timeEntryId=1;
+public class InMemoryTimeEntryRepository implements TimeEntryRepository  {
+    private TimeEntry timeEntry  = new TimeEntry();
+    private HashMap<Long,TimeEntry> timeEntryList = new HashMap();
+    private long timeEntryId =1;
 
     public TimeEntry create(TimeEntry any){
-        this.timeEntry = any;
-        if (any.getId()== 0){
+        System.out.println( "timeEntryId in create "+timeEntryId );
+        this.timeEntry=any;
+        if(any.getId() == 0){
             timeEntry.setId(timeEntryId);
         }
-        timeEntryHashMap.put(timeEntryId, timeEntry);
+        timeEntryList.put(timeEntryId,timeEntry);
         timeEntryId++;
+
         return timeEntry;
     }
-
-    public TimeEntry find(long timeEntryId ){
-        return timeEntryHashMap.get(timeEntryId);
+    public TimeEntry find(Long timeEntryId){
+        System.out.println( "timeEntryId in find "+timeEntryId );
+        return timeEntryList.get(timeEntryId);
+        //return timeEntry;
     }
 
-    @Override
-    public TimeEntry find(Long id) {
-        return null;
+    public List list(){
+
+        return new ArrayList<TimeEntry>(timeEntryList.values());
     }
 
-    public TimeEntry update(long eq,TimeEntry any ){
-        if (timeEntryHashMap.get(eq)== null) {
-                return null;
-            }
-        else
-        {
-            this.timeEntry = any;
-            if (any.getId() == 0) {
+    public void delete(Long timeEntryId){
+
+        timeEntryList.remove(timeEntryId);
+
+    }
+
+    public TimeEntry update(Long eq, TimeEntry any){
+        System.out.println("update in TimeEntry and id is  "+eq);
+        System.out.println("TimeEntry any.getProjectId()  "+any.getProjectId() +"  any.getUserId() = "+ any.getUserId() + " any.getDate() = "+any.getDate() + " any.getHours() = " +any.getHours() );
+
+        if(timeEntryList.get(eq)== null){
+            return null;
+        }else{
+            System.out.println(" getting any.getId() ");
+            System.out.println(" any.getId() " + any.getId());
+            this.timeEntry=any;
+            System.out.println(" Before  any.getId() == 0 " + any.getId());
+            if(any.getId() == 0 ){
+                System.out.println(" In any.getId() == 0 ");
                 timeEntry.setId(eq);
             }
-            timeEntryHashMap.replace(eq, timeEntry);
-
+            System.out.println(" Before replace");
+            timeEntryList.replace(eq,timeEntry);
+            System.out.println(" After replace");
             return timeEntry;
         }
     }
 
-    @Override
-    public TimeEntry update(Long id, TimeEntry timeEntry) {
-        return null;
-    }
-
-    @Override
-    public void delete(Long id) {
-
-    }
-
-    public void delete(long timeEntryId ) {
-        timeEntryHashMap.remove(timeEntryId);
-    }
-
-    public List list( ) {
-        return new ArrayList<TimeEntry>(timeEntryHashMap.values());
-    }
 }
-
